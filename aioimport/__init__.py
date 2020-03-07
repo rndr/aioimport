187724c97080095967cf0f772f3f20cd4d9a905b
rndr <rndr@users.noreply.github.com>
2020-03-07 12:11:03 +0500

    blacked added flake8

--- aioimport/__init__.py
+++ aioimport/__init__.py
@@ -6,24 +6,19 @@ import types
 import typing
 
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
 
 
 class Importer:
-    _executor_factory: typing.Callable[
-        ...,
-        concurrent.futures.ThreadPoolExecutor
-    ]
-    _executor: typing.Optional[
-        concurrent.futures.ThreadPoolExecutor
-    ] = None
+    _executor_factory: typing.Callable[..., concurrent.futures.ThreadPoolExecutor]
+    _executor: typing.Optional[concurrent.futures.ThreadPoolExecutor] = None
 
     def __init__(
         self,
         max_workers: typing.Optional[int] = None,
-        thread_name_prefix: str = '',
+        thread_name_prefix: str = "",
         initializer: typing.Optional[typing.Callable] = None,
-        initargs: typing.Tuple = ()
+        initargs: typing.Tuple = (),
     ) -> None:
         # See https://github.com/python/mypy/issues/708 for type ignore reason
         self._executor_factory = functools.partial(  # type: ignore
@@ -45,24 +40,15 @@ class Importer:
             self._executor = None
 
     async def import_module(
-        self,
-        name: str,
-        package: typing.Optional[str] = None
+        self, name: str, package: typing.Optional[str] = None
     ) -> types.ModuleType:
         executor = self._get_executor()
         return await asyncio.get_event_loop().run_in_executor(
-            executor,
-            functools.partial(
-                importlib.import_module,
-                name,
-                package=package
-            )
+            executor, functools.partial(importlib.import_module, name, package=package)
         )
 
     async def cache_module(
-        self,
-        name: str,
-        package: typing.Optional[str] = None
+        self, name: str, package: typing.Optional[str] = None
     ) -> None:
         await self.import_module(name, package=package)
 
@@ -71,14 +57,10 @@ default = Importer()
 
 
 async def import_module(
-    name: str,
-    package: typing.Optional[str] = None
+    name: str, package: typing.Optional[str] = None
 ) -> types.ModuleType:
     return await default.import_module(name, package=package)
 
 
-async def cache_module(
-    name: str,
-    package: typing.Optional[str] = None
-) -> None:
+async def cache_module(name: str, package: typing.Optional[str] = None) -> None:
     await default.cache_module(name, package=package)
